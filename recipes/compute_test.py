179003ea6698a2a1bf4b28b804394369cd017411
TarepSH <TarepSH@gmail.com>
2022-03-09 10:37:45 +0200

    Saved recipe: SARC_HQ2.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -324,8 +324,7 @@ def sedning_email(replyFor, subject,results,counts_of_check_status_open_balnce, 
     msg.attach(part)
     msg.attach(part2)
 
-    server = smtplib.SMTP('smtp.office365.com', 587)
-    server.ehlo()
+    server = smtplib.starttls('smtp.office365.com', 587)
     server.starttls()
 
     # Login Credentials for sending the mail
