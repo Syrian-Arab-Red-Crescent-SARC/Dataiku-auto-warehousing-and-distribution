351e79358f574da191b7f7b40588200c580b4a43
TarepSH <TarepSH@gmail.com>
2022-05-23 13:10:15 +0200

    Saved recipe: SARC_HQ2.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -143,8 +143,9 @@ def old_war_check():
     old_war_df = war_to_check.get_dataframe()
 
     #adding and Testing
-    #war_to_check_open_totlo = dataiku.Dataset("wearhouse_row_compning_ok_month_prepared")
-    #old_war_df_check = war_to_check_open_totlo.get_dataframe()
+    project.get_dataset("wearhouse_row_compning_ok_month_prepared")
+    war_to_check_open_totlo = dataiku.Dataset("wearhouse_row_compning_ok_month_prepared")
+    old_war_df_check = war_to_check_open_totlo.get_dataframe()
 
 
     war_to_check_total_out = dataiku.Dataset("wearhouse_row_data_for_check_wiht_dis")
@@ -157,7 +158,10 @@ def old_war_check():
     #set the variables
     counts_of_check_status_open_balnce = old_war_df['check_status_open_balnce'].value_counts()
     counts_of_check_status = old_war_df['check_status'].value_counts()
-    total_sum_of_closing_sum_for_old = old_war_df['old_Closing_Balance_sum'].sum()
+    #testing start
+    total_sum_of_closing_sum_for_old_search = old_war_df_check.where(old_war_df_check['Branch']==empty_war_df['Branch'].values[0])
+    total_sum_of_clsogin_sum_for_old = total_sum_of_closing_sum_for_old_search['Closing_Balance'].sum()
+    #testin edn
     total_sum_of_open_balnce_for_now = old_war_df['Open_Balance_sum'].sum()
     total_sum_of_out_to_check_from_war = war_total_out['Total_out_sum'].sum()
     #check that all the items total from previous month is there
