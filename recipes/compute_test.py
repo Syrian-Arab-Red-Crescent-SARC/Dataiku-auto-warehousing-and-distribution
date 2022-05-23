839e6a5ea4e4bc651d11ba69d241aa5478861959
TarepSH <TarepSH@gmail.com>
2022-05-23 14:10:52 +0200

    Saved recipe: SARC_HQ2.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -159,7 +159,7 @@ def old_war_check():
     counts_of_check_status_open_balnce = old_war_df['check_status_open_balnce'].value_counts()
     counts_of_check_status = old_war_df['check_status'].value_counts()
     #testing start
-    total_sum_of_closing_sum_for_old_search = old_war_df_check.where(old_war_df_check['Branch']== war_total_out['Branch'].values[0])
+    total_sum_of_closing_sum_for_old_search = old_war_df_check.where(old_war_df_check['Branch']== empty_war_df['Branch'].values[0])
     total_sum_of_closing_sum_for_old = 0
     #testin edn
     total_sum_of_open_balnce_for_now = old_war_df['Open_Balance_sum'].sum()
